fd68c63cd746f447f398c6bb99bfa237f4b3e508
paulovitornovaes <paulonovaes@id.uff.br>
2021-07-17 16:57:23 -0300

    preciso fazer struc para passar o tamanho do vetor

--- part_2/main.c
+++ part_2/main.c
@@ -2,44 +2,89 @@
 #include <pthread.h>
 #include <stdlib.h>
 #include <time.h>
-#define MAX 5
+#include <semaphore.h>
+#define MAX 3
 
-void *remove_par(void *arg){
-    int numero = (int *) arg;
+int tamanho = MAX;
 
+//FUNÇÃO SEM SEMAFORO, TIROU OS NUMEROS PARES CERTINHO
+void *remove_par_only_thread(void *arg)
+{
+
+    
+    int *numero = (int *)arg;
     //looping reverso
     for (int i = MAX - 1; i >= 0; i--)
     {
-        /* code */
+        if (numero[i] % 2 == 0)
+        {
+            for (int j = i; j < MAX; j++)
+            {
+                numero[j] = numero[j + 1];
+                tamanho--;
+            }
+        }
     }
-    
-    
-    
-    
-    
+    return NULL;
+    //funcionou OK
+}
+
+//FUNÇÃO COM THREAD E SEM SEMAFORO, TIROU OS NUMEROS PARES CERTINHO
+void *remove_par(void *arg)
+{
+    int *numero = (int *)arg;
+    //looping reverso
+    for (int i = MAX - 1; i >= 0; i--)
+    {
+        if (numero[i] % 2 == 0)
+        {
+            for (int j = i; j < MAX; j++)
+            {
+                numero[j] = numero[j + 1];
+            }
+        }
+    }
+}
+
+void *remove_multiplo_only_thread(void *arg)
+{
+    int *numero = (int *)arg;
+
 }
 
 int main()
 {
+    //criando tipo thread
     pthread_t thread_1, thread_2;
+
+    //criando um vetor de inteiros
     int vetor_inteiros[MAX];
 
+    //iniciando o semaforo
+
+    //chamada do srand para auxiliar na criação de numeros randomicos
     srand(time(NULL));
+
+    //looping de indice 0 até o valor MAX
+    printf("Vetor de inteiros MAIN ->\n");
     for (int i = 0; i < MAX; i++)
     {
         //vetor de inteiros recebe o numero aleatorio de 0 a 100
         vetor_inteiros[i] = rand() % 100 + 1;
-        printf("vetor -> [%d]\n", vetor_inteiros[i]);
+
+        printf("[%d]\n", vetor_inteiros[i]);
     }
 
     printf("\n");
 
-    //looping reverso
-    for (int i = MAX - 1; i >= 0; i--)
+    int tamanho_novo = (int *) (pthread_create(&thread_1, NULL, &remove_par_only_thread, (void *)vetor_inteiros));
+
+    printf("TAMANHO MAIN = %d\n", tamanho_novo);
+
+    printf("Vetor com a função que remove pares->\n");
+    for (int i = 0; i < MAX; i++)
     {
-        int *pvetor_inteiros = vetor_inteiros[i];
-        
-        pthread_create(&thread_1, NULL, &remove_par,(void *)pvetor_inteiros);
+        printf("[%d]\n", vetor_inteiros[i]);
     }
 
     return 0;
