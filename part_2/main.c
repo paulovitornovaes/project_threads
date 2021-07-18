ad766eef8ff2cb4b36ed94e23327d9756ad4c71e
paulovitornovaes <paulonovaes@id.uff.br>
2021-07-18 18:45:43 -0300

    consegui funcao remove_par

--- part_2/main.c
+++ part_2/main.c
@@ -3,68 +3,39 @@
 #include <stdlib.h>
 #include <time.h>
 #include <semaphore.h>
-#define MAX 3
+#define MAX 5
 
-int tamanho = MAX;
-
-//FUNÇÃO SEM SEMAFORO, TIROU OS NUMEROS PARES CERTINHO
-void *remove_par_only_thread(void *arg)
+struct vetor_dados
 {
-
-    
-    int *numero = (int *)arg;
-    //looping reverso
-    for (int i = MAX - 1; i >= 0; i--)
-    {
-        if (numero[i] % 2 == 0)
-        {
-            for (int j = i; j < MAX; j++)
-            {
-                numero[j] = numero[j + 1];
-                tamanho--;
-            }
-        }
-    }
-    return NULL;
-    //funcionou OK
-}
+    int vetor[MAX];
+    int tamanho;
+};
 
 //FUNÇÃO COM THREAD E SEM SEMAFORO, TIROU OS NUMEROS PARES CERTINHO
 void *remove_par(void *arg)
 {
-    int *numero = (int *)arg;
+    struct vetor_dados *arg_dados = (struct vetor_dados *)arg;
+
     //looping reverso
     for (int i = MAX - 1; i >= 0; i--)
     {
-        if (numero[i] % 2 == 0)
+        if (arg_dados->vetor[i] % 2 == 0)
         {
+            arg_dados->tamanho--;
             for (int j = i; j < MAX; j++)
             {
-                numero[j] = numero[j + 1];
+                arg_dados->vetor[j] = arg_dados->vetor[j + 1];
             }
         }
     }
+
+    return NULL;
 }
 
-void *remove_multiplo_only_thread(void *arg)
+void *gera_vetor_random(int *vetor_inteiros)
 {
-    int *numero = (int *)arg;
-
-}
-
-int main()
-{
-    //criando tipo thread
-    pthread_t thread_1, thread_2;
-
-    //criando um vetor de inteiros
-    int vetor_inteiros[MAX];
-
-    //iniciando o semaforo
-
     //chamada do srand para auxiliar na criação de numeros randomicos
     srand(time(NULL));
-
     //looping de indice 0 até o valor MAX
     printf("Vetor de inteiros MAIN ->\n");
     for (int i = 0; i < MAX; i++)
@@ -74,17 +45,26 @@ int main()
 
         printf("[%d]\n", vetor_inteiros[i]);
     }
-
     printf("\n");
 
-    int tamanho_novo = (int *) (pthread_create(&thread_1, NULL, &remove_par_only_thread, (void *)vetor_inteiros));
+        return NULL;
+}
 
-    printf("TAMANHO MAIN = %d\n", tamanho_novo);
+int main()
+{
+    pthread_t thread_1, thread_2;
+    struct vetor_dados vetor_inteiros;
+    vetor_inteiros.tamanho = MAX;
+    int *pvetor_inteiros = vetor_inteiros.vetor;
+
+    gera_vetor_random(pvetor_inteiros);
+
+    pthread_create(&thread_1, NULL, &remove_par, &vetor_inteiros);
 
     printf("Vetor com a função que remove pares->\n");
-    for (int i = 0; i < MAX; i++)
+    for (int i = 0; i < vetor_inteiros.tamanho; i++)
     {
-        printf("[%d]\n", vetor_inteiros[i]);
+        printf("[%d]\n", vetor_inteiros.vetor[i]);
     }
 
     return 0;
