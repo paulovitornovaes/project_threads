8e8be4f375c672bb7b906cf0971afb1089dae377
paulovitor5656 <66034241+paulovitor5656@users.noreply.github.com>
2021-08-05 18:19:03 -0300

    final

--- part_1/main.c
+++ part_1/main.c
@@ -3,105 +3,62 @@
 #include <string.h>
 #include <sys/syscall.h>
 
-#define MAX 10000
+#define MAX 100000
 
-//função itoa que transforma int em string TALVEZ EU RETIRE PARA USAR SPRINTF
-
-char *itoa(int i, char b[])
-{
-    char const digit[] = "0123456789";
-    char *p = b;
-    if (i < 0)
-    {
-        *p++ = '-';
-        i *= -1;
-    }
-    int shifter = i;
-    do
-    { //Move to where representation ends
-        ++p;
-        shifter = shifter / 10;
-    } while (shifter);
-    *p = '\0';
-    do
-    { //Move back, inserting digits as u go
-        *--p = digit[i % 10];
-        i = i / 10;
-    } while (i);
-    return b;
-}
 
 void *rotina(void *arg)
 {
     char *string = (char *) arg;
-    printf("Olá, eu sou a %s de id %lu \n", string, pthread_self());
+    //o argumento que foi castado para void agora tem que ser castado para string pois só assim poderemos printar o nome da thread
+    printf("Print do pthread_self...\n");
+    printf("Olá, eu sou a %s de id %u\n", string, pthread_self());
+    printf("\n");
+    printf("Print do gettid...\n");
+    printf("Olá, eu sou a %s de id %u \n", string, gettid());
+    // a diferença entre pthread_self e gettid é que o primeiro é criado o id pelo proprio pthread_self, já o gettid cria o id com uma chamada do sistema
     
 }
 
-/*
-int main2(int argc, char *argv[])
-{
-    int n;
-    pthread_t threads[MAX];
 
-    printf("digite a quantidade de threads a serem criadas = ");
-    scanf("%d", &n);
-
-    //looping para fazer a quantidade de threads necessárias
-    for (int i = 0; i < n; i++)
-    {
-        //vetores que fiz para criar o nome da thread
-        char name[MAX] = "Thread_";
-        char number[MAX];
-
-        //inteiro i transformado em char e jogado para o vetor number
-        itoa(i, number);
-
-        //concatenar as strings name e number (vai ficar salvo no primeiro parametro)
-        strcat(name, number);
-
-        //passando o vetor name como ponteiro
-        char *pname = name;
-
-        //criação da thread na posição i do vetor, passando a rotina e o argumento ponteiro pname que aponta para o name[MAX]
-        pthread_create(&threads[i], NULL, &rotina, pname);
-
-        //limpar o vetor para nao ficar adicionando o numero (ex thread_012345...)
-        memset(name, 0, MAX);
-    }   
-
-    
-    for (int i = 0; i < n; i++){
-        pthread_join(threads[i], NULL);
-    }
-
-
-    return 0;
-}
-*/
 int main()
 {
     int n;
+
+    //vetor para guardar as threads
     pthread_t threads[MAX];
-    char names[10][MAX];
+
     printf("Digite a quantidade desejada de threads = ");
     scanf("%d", &n);
+    char names[n][50];
+
 
     for (int i = 0; i < n; i++)
     {
+        //laço que vai de 0 até a quantidade de threads que o usuário desejou
+        //vetor para guardar a string thread_
         char name_thread[] = "Thread_";
+        //vetor que vai guardar os numeros gerados de cada thread no laço
         char number_thread[MAX];
+        //uso do sprintf para converter o int para char
         sprintf(number_thread, "%d", i + 1);
+        //concatenar o name_thread com number_thread
         strcat(name_thread, number_thread);
+        //criado um ponteiro com o thread_numero
         char *pnameThread = name_thread;
+        //o vetor de vetores agora recebe o nome formado
         strcpy(names[i], pnameThread);
 
     }
     
     for (int i = 0; i < n; i++)
     {
-       
+        //laço que vai de 0 até a quantidade de threads que o usuário desejou
+        //ponteiro que recebe o nome da thread na posição i
         char *name_to_thread = names[i];
+        //chamada da função pthread_create
+        //cria uma thread e coloca no vetor de threads
+        // a thread[i] vai executar a função rotina 
+        //name_to_thread é passado para uma função do tipo void, então tem que ser feito o casting para void
         pthread_create(&threads[i], NULL, rotina, (void *)name_to_thread);
     }
     
